ddba7a62b5b1d5d1692e8f12184eaae8626a63ea
Philip Salire <philip.s.salire@gmail.com>
2019-04-11 00:40:45 -0700

    more info

--- fantasy5stats.py
+++ fantasy5stats.py
@@ -17,43 +17,59 @@ def get_file():
     txt_file = txt_file.replace(r'\n', '\n')
     return txt_file
 
-def print_stats(histogram, tot):
-    print("Total: {}".format(tot))
-    for val in histogram:
-        print("{}: {} ({:.3f}%)".format(val[0], val[1], (val[1] / tot)*100))
-
-def main():
-    args = get_args()
-
-    # Get txt file
-    lotto_file = get_file()
-
-    lines = lotto_file.split('\n')
-
+def build_histogram_and_write_to_file(lines, out_file):
     # Initialize histogram
     histogram = {}
     for i in range(39):
         histogram[str(i + 1)] = 0
 
-    # Extract numbers from txt file to build histogram & save raw_numbers file
-    if args.nosave == False:
-        raw_numbers = open(args.filename[0], 'w')
-    for line in lines[5:]:
+    for line in lines:
         numbers = re.findall(r'\d+', line)
         for i, word in enumerate(numbers[3:]):
             histogram[word] += 1
-            if args.nosave == False:
-                raw_numbers.write(word)
+            if out_file is not None:
+                out_file.write(word)
                 if i < 4:
-                    raw_numbers.write(' ')
+                    out_file.write(' ')
                 else:
-                    raw_numbers.write('\r\n')
+                    out_file.write('\r\n')
+    return histogram
+
+def print_stats(histogram_items, histogram_dict, tot, max_val, min_val, current_numbers):
+    for val in histogram_items:
+        print("{}: {} ({:.3f}%)".format(val[0], val[1], (val[1] / tot)*100))
+    print("")
+    print("Total Numbers: {}".format(tot))
+    print("Max Frequency: {}: {} ({:.3f}%)".format(max_val[0], max_val[1], (max_val[1] / tot)*100))
+    print("Min. Frequency: {}: {} ({:.3f}%)\n".format(min_val[0], min_val[1], (min_val[1] / tot)*100))
+    print("Current Winning Numbers: {}".format(" ".join(current_numbers)))
+    for num in current_numbers:
+        print("{}: {} ({:.3f}%)".format(num, histogram_dict[num], (histogram_dict[num] / tot)*100))
+
+#### MAIN ####
+def main():
+    args = get_args()
+    
+    print("----------\nFantasy 5\n----------")
+
+    # Get txt file
+    lotto_file = get_file()
+    lines = lotto_file.split('\n')
+    
+    # Extract numbers from txt file to build histogram & save raw_numbers file
+    raw_numbers = None
+    if args.nosave == False:
+        raw_numbers = open(args.filename[0], 'w')
+    histogram = build_histogram_and_write_to_file(lines[5:], raw_numbers)
     if args.nosave == False:
         raw_numbers.close()
 
     # Sort histogram
+    hist_ascend = sorted(histogram.items(), key=lambda x: x[1])
+    min_val = hist_ascend[0]
+    max_val = hist_ascend[-1]
     if args.ascending == True or args.descending == True:
-        sorted_hist = sorted(histogram.items(), key=lambda x: x[1])
+        sorted_hist = hist_ascend
         if args.descending == True:
             sorted_hist.reverse() # Descending order
     else:
@@ -64,7 +80,7 @@ def main():
     for val in sorted_hist:
         tot += val[1]
 
-    print_stats(sorted_hist, tot)
+    print_stats(sorted_hist, histogram, tot, max_val, min_val, re.findall(r'\d+', lines[5])[3:])
 
 if __name__ == "__main__":
     main()
